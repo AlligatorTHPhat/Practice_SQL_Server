5d10ac0ee38baed1226779c9f6323f79d5f181e7
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-12-05 23:41:18 +0700

    Add files via upload

---
+++ RUNCODE.sql
@@ -0,0 +1,15 @@
+﻿--CAU1
+EXEC CAU1 @LOAIGPLX = 'B2'
+--CAU2
+--INSERT INTO BangLai (MaBangLai, MaTaiXe, NgayCap, LoaiGPLX)
+--VALUES ('ASSH', 'X001', '2024-01-01', 'C'); 
+
+INSERT INTO BangLai (MaBangLai, MaTaiXe, NgayCap, LoaiGPLX)
+VALUES ('M013', 'X005', '2010-01-01', 'B2'); -- Lỗi (tuổi ≤ 20)
+
+
+--cau3
+--INSERT INTO BangLai (MaBangLai, MaTaiXe, NgayCap, LoaiGPLX)
+--VALUES ('A001', 'X001', '2024-01-01', 'C');
+
+--SELECT * FROM TaiXe;
---
+++ SQLQuery4.sql
@@ -0,0 +1,24 @@
+﻿USE [dbXe]
+GO
+
+CREATE TRIGGER TRG_CAU2
+ON BANGLAI
+
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF EXISTS (
+        SELECT 1
+        FROM BANGLAI BL
+        INNER JOIN INSERTED I ON BL.MABANGLAI = I.MaBangLai
+        INNER JOIN TAIXE TX ON BL.MATAIXE = TX.MATAIXE
+        WHERE (YEAR(I.NGAYCAP) - YEAR(TX.NGAYSINH)) <= 20
+    )
+    BEGIN
+        RAISERROR (N'Không hợp lệ! Tài xế phải trên 20 tuổi tại thời điểm cấp bằng lái!', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END;
+GO
